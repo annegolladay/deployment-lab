7a55d7e30b6a88b3ea1c4a07a7695984d0c50970
Anne Golladay <annegolladay@gmail.com>
2021-06-10 14:46:39 -0600

    fixing errors in index.js for server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const path = require('path')
-
 const app = express()
+const port = process.env.PORT || 4005
+
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../index.html'))
 })
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../index.css'))
+  })
+app.get('/images', function(req, res) {
+    res.sendFile(path.join(__dirname, '../1akdbeach_img.jpg'))
+})
 
-const port = process.env.PORT || 4005
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
